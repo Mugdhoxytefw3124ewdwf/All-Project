e5ce28d8dee552c640fe26e2dd80ef36e0f5a90b
Mugdhoxytefw3124ewdwf <163859674+Mugdhoxytefw3124ewdwf@users.noreply.github.com>
2024-04-17 18:30:09 +0600

    Update calculator_project..py

--- calculator_project..py
+++ calculator_project..py
@@ -1,20 +1,20 @@
 import math # import mathamatis libary.
 
 
-Add = lambda a, b: a + b
-subtract = lambda x, y: x - y
-multyply = lambda x, y: x * y
-divide = lambda x, y: x / y
-sqare_root = lambda x: math.sqrt(x)
-
+Add = lambda a, b: a + b #Creat function to Num
+subtract = lambda x, y: x - y #Creat function to subtrac
+multyply = lambda x, y: x * y #Multply *
+divide = lambda x, y: x / y #divide /
+sqare_root = lambda x: math.sqrt(x) Creat function to find Sqare root 
+# Show all operation's name 
 print("\n 1. Add \n 2. subtract \n 3. multyply \n 4. divide \n 5. sqare_root \n 6. Done")
 print("if your mison is sqare root we will root only your first input")
-
+#Creat loop for get input forever , if user commend to stop then stop.
 while True:
-    choise = int(input("Type here your mison : "))
-    x = int(input("Type here your Digit and press enter: "))
-    y = int(input("Type here your 2nd Digit and press enter: "))
-    
+    choise = int(input("Type here your mison : ")) #get user choise to know thir operation
+    x = int(input("Type here your Digit and press enter: ")) #get oppend
+    y = int(input("Type here your 2nd Digit and press enter: ")) #get other oprend
+# Cheak the user input and complete thir operation by  condition    
     if choise == 1:
         print(f"Your result is {Add(x, y)}")
 
